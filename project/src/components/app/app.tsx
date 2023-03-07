abe902a5c423ce3feaa04e57f6faa8dc663e5bd8
Roman Trukhan <imbrickiy@ya.ru>
2023-03-07 13:10:02 +0300

    Создана главная страница, компонент карточки. Добавлен пропс по количеству жилья

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,9 @@
+import MainScreen from '../../pages/main/main';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <MainScreen placesFound={312}/>
+  );
 }
 
 export default App;
