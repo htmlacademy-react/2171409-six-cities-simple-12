386862ad297add310c880ebcdb9d9c6771a9b01a
Roman Trukhan <imbrickiy@ya.ru>
2023-03-13 16:11:51 +0300

    Создан отдельный компонент 404 страницы.добавлена маршрутизация

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,13 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainScreen from '../../pages/main/main';
-
+import Login from '../../pages/login/login';
+import ErrorScreen from '../../pages/404/404';
+import PropertyScreen from '../../pages/property/property';
 
 type AppProps = {
   placesAmount: number;
 }
 
-function App({placesAmount}: AppProps ): JSX.Element {
+function App({ placesAmount }: AppProps): JSX.Element {
   return (
-    <MainScreen placesAmount={placesAmount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainScreen placesAmount={placesAmount} />} />
+        <Route path={AppRoute.Login} element={<Login />} />
+        <Route path={AppRoute.Room} element={< PropertyScreen />} />
+        <Route path='*' element={<ErrorScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
